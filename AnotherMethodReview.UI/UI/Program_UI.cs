a5170c3f24fb43c102603ecb0bb7af6b5a5b8e4f
Terry Brown <efa.terrybrown@gmail.com>
2022-04-23 08:31:30 -0400

    InitialCommit

---
+++ AnotherMethodReview.UI/UI/Program_UI.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//https://github.com/EFA-TerryBrown/Methods_Review
+public class Program_UI
+{
+    // Make a method that takes in a string and Greets the user
+
+
+    // Make a method that takes in two integers and returns the added result
+
+
+    // Make a method that takes in two decimal and returns the subtracted result as an integer
+
+
+
+    // Make a method that takes in two doubles and returns the Multiplied result
+
+
+    // Make a method that takes in a person object and returns all of the details of the person
+
+    // Make a method that takes in a number and outputs a string that states wheather or not the number
+    // is evenly divisble by 5
+
+    // Make a mehtod that takes in two interger values, one for the minValue and one for the maxValue.
+    // Next, using a nexted loop that displays times tables from 1 to 5.
+    // 1 X 1 = 1
+
+    // ….
+
+    // 1 X 12 = 12
+
+
+    // Declare a method that takes in a list of Person and return the sum of all Salaries on the list.
+
+}
